8f1ebc7d9cf637bbabe4ab6c8a0c246cf9a7c8b2
berna_cabrera@hotmail.com <berna_cabrera@hotmail.com>
2020-08-29 13:25:20 -0500

    segunda ronda de commits

---
+++ src/test/java/ApiTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import static org.junit.Assert.*;
+import static io.restassured.RestAssured.*;
+
+
+public class ApiTest {
+
+    @Test
+    public void coronavirus() {
+
+        RestAssured.baseURI = String.format("https://api.quarantine.country/api/v1/summary/latest");
+
+        Response response = given()
+                .header("Accept", "application/json")
+                .log().all()
+                .get();
+
+        assertEquals(200, response.getStatusCode());
+
+        String body = response.getBody().asString();
+        System.out.println("Body response: " + body);
+
+    }
+}
